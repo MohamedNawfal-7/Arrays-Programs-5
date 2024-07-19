843f65968a17d36a5d67966a0ca65ec128a4c7f9
Mohamed Nawfal <mohamednawfal183@gmail.com>
2024-07-19 17:04:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1.Deduplication
+// // let arr=[1,1,2,3,4,4,5,5]
+// // let sort=[...new Set(arr)]
+// // document.write(sort)
+// 2.Array spreading
+// let name=['nawfal','pilot','engineer']
+// let call=[...name,'doctor']
+// document.write(call)
+// 3.merge two array
+// let arr=[1,2,3,4,5]
+// let arr1=[6,7,8,9,10]
+// let merge=[...new Set([...arr,...arr1])]
+// document.write(merge)
+// 4.findof
+// let name1=['nawfal','pilot','b.tech it']
+// let call=name1.find(name=>name.startsWith('p'))
+// document.write(call)
+// 5.Grouping Array
+// let arr=[1,2,3,4,5]
+// let arr1=[6,7,8,9,10]
+// let call=[...arr,...arr1]
+// document.write(call)
